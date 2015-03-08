dea3e0627507090375d2b581fdb3813b32242ce4
ramahadevan <ramahadevan@ebay.com>
2015-03-07 17:33:29 -0800

    upgraded 'dustjs-linkedin' to 2.5 and fixed the test cases

--- test/test.js
+++ test/test.js
@@ -6,26 +6,18 @@ var expect = require('chai').expect;
 var nodePath = require('path');
 var compiler = require('../');
 
-describe('dustc-commonjs' , function() {
-
+describe('dustc-commonjs', function() {
     beforeEach(function(done) {
-        // for (var k in require.cache) {
-        //     if (require.cache.hasOwnProperty(k)) {
-        //         delete require.cache[k];
-        //     }
-        // }
-
         done();
     });
 
-    it('should render a raptor template with a callback', function(done) {
+    it('should render a dust template with a callback', function(done) {
         compiler.compileFile(nodePath.join(__dirname, 'test.dust'), function(err, src) {
             if (err) {
                 return done(err);
             }
-            expect(src).to.equal('(function(){module.exports=body_0;function body_0(chk,ctx){return chk.write("Hello ").reference(ctx.get(["name"], false),ctx,"h").write("!");}return body_0;})();');
+            expect(src).to.equal('(function(){module.exports=body_0;function body_0(chk,ctx){return chk.w("Hello ").f(ctx.get(["name"], false),ctx,"h").w("!");}body_0.__dustBody=!0;return body_0;})();');
             done();
         });
     });
 });
-
